ed2177be4fa6f6b2ee852e13d120d9405ac6e56e
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-19 14:44:00 +0700

    upload

---
+++ src/pboif2_10119065_latihan15_logika/PBOIF2_10119065_Latihan15_Logika.java
@@ -0,0 +1,21 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini berisi Logika
+ *
+ */
+package pboif2_10119065_latihan15_logika;
+
+public class PBOIF2_10119065_Latihan15_Logika {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
