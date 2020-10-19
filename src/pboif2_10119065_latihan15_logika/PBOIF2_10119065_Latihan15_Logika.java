5831dfc3ad0157c1a4c51e09b8427b1760b53123
Fachriansyah MNI <37337274+fachriansyahmni@users.noreply.github.com>
2020-10-19 15:26:56 +0700

    Update PBOIF2_10119065_Latihan15_Logika.java

--- src/pboif2_10119065_latihan15_logika/PBOIF2_10119065_Latihan15_Logika.java
+++ src/pboif2_10119065_latihan15_logika/PBOIF2_10119065_Latihan15_Logika.java
@@ -1,3 +1,4 @@
+package pboif2_10119065_latihan15_logika;
 /*
  * @author
  * NAMA                 : Fachriansyah Muhammad Nur Ihsan
@@ -6,7 +7,6 @@
  * Deskripsi Program    : Program ini berisi Logika
  *
  */
-package pboif2_10119065_latihan15_logika;
 
 public class PBOIF2_10119065_Latihan15_Logika {
     public static void main(String[] args) {
